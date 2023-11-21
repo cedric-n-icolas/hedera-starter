061362ebf657cbc7a2f7ac32e53ea1a9952a7870
Cedric NICOLAS <cedric@stork.express>
2023-11-21 17:50:39 +0100

    change deploy method

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,13 @@
 const hre = require("hardhat");
-
+require('dotenv').config();
 async function main() {
-
+  
   // Deploy the contract
   const HelloHedera = await hre.ethers.getContractFactory("HelloHedera");
-  const helloHedera = await HelloHedera.deploy();
+  console.log("EVM_ADDRESS= "+process.env.EVM_ADDRESS);
+  const helloHedera = await HelloHedera.deploy(process.env.EVM_ADDRESS);
 
-  const contractAddress = (await helloHedera.deployTransaction.wait()).contractAddress;
+  const contractAddress = (await helloHedera.deploymentTransaction().wait()).contractAddress;
 
   console.log("HelloWorld contract deployed to:", contractAddress);
 
